6894256013047a1b53141ec17bb6b191e09e8923
SGHOSHROY <72212934+SGHOSHROY@users.noreply.github.com>
2022-08-05 09:58:57 +0530

    Create factorial.cpp
    C++ to print factorial of number

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int a)
+{
+    if(n==0||n==1)
+        return 1;
+    
+    return a*factorial(a-1);
+}
+
+int main()
+{
+    int a=6;
+     cout<<factorial(a);
+}
